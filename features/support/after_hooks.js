e50182ae59dc61bb35c2b762c387af59cec67ed2
Gayathri Mohan <gayathrimohan88@gmail.com>
2016-04-05 17:21:43 +0530

    capture screenshot on failure

--- features/support/after_hooks.js
+++ features/support/after_hooks.js
@@ -1,5 +1,12 @@
-export default function () {
-  this.After(function (scenario, done) {
-    this.driver.quit().nodeify(done);
+import steps from 'artstep';
+import fs from 'fs';
+
+export default steps()
+  .after(async function (scenario) {
+    if (scenario.isFailed()) {
+      const buffer = await this.driver.context('NATIVE_APP').takeScreenshot();
+      const filePath = `${__dirname}/../../reports/screenshots/${scenario.getName()}.png`;
+      await fs.writeFile(filePath, buffer, 'base64');
+    }
+    await this.driver.quit();
   });
-}
--- features/support/caps.js
+++ features/support/caps.js
@@ -6,5 +6,11 @@ export default {
       deviceName: 'Android Emulator',
       app: `${__dirname}/../../apps/android-debug.apk`,
     },
+    5.1: {
+      platformName: 'Android',
+      platformVersion: '5.1',
+      deviceName: 'Android Phone',
+      app: `${__dirname}/../../apps/android-debug.apk`,
+    },
   },
 };
