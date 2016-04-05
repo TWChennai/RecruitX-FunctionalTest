bb82883424244d867bb63de2fbe2dbdfda8113b7
Gayathri Mohan <gayathrimohan88@gmail.com>
2016-04-05 22:58:35 +0530

    Attaching screenshot to cucumber html report

--- features/support/after_hooks.js
+++ features/support/after_hooks.js
@@ -1,12 +1,10 @@
 import steps from 'artstep';
-import fs from 'fs';
 
 export default steps()
   .after(async function (scenario) {
     if (scenario.isFailed()) {
       const buffer = await this.driver.context('NATIVE_APP').takeScreenshot();
-      const filePath = `${__dirname}/../../reports/screenshots/${scenario.getName()}.png`;
-      await fs.writeFile(filePath, buffer, 'base64');
+      await scenario.attach(new Buffer(buffer, 'base64').toString('binary'), 'image/png');
     }
     await this.driver.quit();
   });
